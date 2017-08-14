3cded00700e181b88973f4c8ad35ce3af6605801
dcalsky <dcalsky@163.com>
2017-08-14 23:51:21 +0800

    chore(email script): update email development start script as same as others

--- start-email.sh
+++ start-email.sh
@@ -2,4 +2,4 @@
 
 # Email application development env shell script
 # TODO: Email container can't be started by docker-compose's up command 
-docker-compose -f docker-compose-dev.yml run --rm email
+docker-compose -f docker-compose-dev.yml up email
